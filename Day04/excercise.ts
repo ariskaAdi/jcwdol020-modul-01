74d3237725f904cb303a01bc1352b068cca63634
ariskaAdi <ariskaadiprasetyo@gmail.com>
2025-04-24 21:58:27 +0700

    soal 4-6

--- Day04/excercise.ts
+++ Day04/excercise.ts
@@ -1,14 +1,16 @@
 // soal slide 3
 // soal 1
 
-let angka: number = 9;
-for (let i: number = 1; i < angka; i++) {
+let angka: number = 4;
+let maksAngka: number = 10;
+
+for (let i: number = 1; i <= maksAngka; i++) {
   console.log(`${angka} x ${i}`);
 }
 
 // soal 2
 
-let kata: string = "aku";
+let kata: string = "madam";
 let kebalikan = kata.split("").reverse().join("");
 if (kata === kebalikan) {
   console.log("palindrome");
@@ -17,9 +19,11 @@ if (kata === kebalikan) {
 }
 
 // soal 3
-let misalJarak: number = 1000;
+let misalJarak: number = 5400;
 if (misalJarak >= 1000) {
   console.log(`${misalJarak / 1000} km`);
+} else {
+  console.log(`${misalJarak} meter`);
 }
 
 // soal 4
@@ -29,4 +33,33 @@ console.log(
     style: "currency",
     currency: "IDR",
   })
+);
+
+// soal 5
+
+let kataPertama: string = "hello";
+let searchString: string = "ell";
+let kataKedua: string = "world";
+let hasilnya: string = "";
+
+if (kataPertama.includes(searchString)) {
+  let hasil: string = kataPertama.replace(searchString, "");
+  hasilnya = `${hasil} ${kataKedua}`;
+} else {
+  console.log(`${kataPertama} ${""} ${kataKedua}`);
+}
+
+console.log(hasilnya);
+
+// soal 6
+
+let firstWord: string = "hello";
+let secondWord: string = "world";
+const sentence = `${firstWord} ${secondWord}`;
+
+console.log(
+  `${sentence
+    .split(" ")
+    .map((i) => i.charAt(0).toUpperCase() + i.slice(1))
+    .join(" ")}`
 );
